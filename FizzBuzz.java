38d80bf7b58140cb2c43817c5fb092ad1e659539
Tanner Keyes <Tanner.Keyes@stu.jefferson.kyschools.us>
2015-11-18 11:06:29 -0500

    Describe tests for FizzBuzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,5 +1,22 @@
+/*
+Tanner Keyes
+*/
+
 class FizzBuzz {
   //Body of FizzBuzz class
   //will inlude methods for computing a single FizzBuzz number
   //and a list of n FizzBuzz numbers
+
+
+	/* Fizz Number (int n)
+		compute the proper fizzbuzz number for number n
+	*/
+
+
+	/*Fizz List (int start, int end)
+	   creates a string containing a list
+	   a list of all fizzbuzz outputs starting with
+	   start and ending with end -1.
+	 */
+
 }
--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -1,6 +1,43 @@
+/*
+Tanner Keyes
+*/
 class FizzBuzzTest {
   //FizzBuzz test class
   //A program to test the functionality of our FizzBuzz class
+  public static void fizzNumberTest() {
+  	//n=0
+
+  	//n=1
+
+  	//n=3
+
+  	//n=5
+
+  	//n=15
+
+  	//n="string"
+
+  	//n="-9"
+
+  	//n="-10"
+
+  	//n="-15"
+
+  	//n=101
+
+
+  }
+
+  public static void fizzListTest() {
+  	//start n = 1 end = 5
+  	//start n = 12 end = 16
+  	//start n = -2 end = 2
+  	//start n = 10 end = 5
+
+
+
+  }
+
   public static void main(String[] args) {
     //Program logic goes here
   }
